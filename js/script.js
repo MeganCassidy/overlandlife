02a03476fab8314836df230a90679be003ea5644
Megan Cassidy <61455706+MeganCassidy@users.noreply.github.com>
2020-10-11 23:19:15 +0100

    further calendar updates

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ var endYear = 2025;
 var month = 0;
 var year = 0;
 
+var selectedDays = new Array();
+var mousedown = false;
+
+
 function loadCalendarMonths() {
     for (var i = 0; i < months.length; i++) {
         var doc = document.createElement("div");
@@ -91,3 +95,52 @@ window.addEventListener('load', function () {
     loadCalendarYears();
     loadCalendarDays();
 });
+
+function loadCalendarDays() {
+    document.getElementById("calendarDays").innerHTML = "";
+
+    var tmpDate = new Date(year, month, 0);
+    var num = daysInMonth(month, year);
+    var dayofweek = tmpDate.getDay();       // find where to start calendar day of week
+
+    for (var i = 0; i <= dayofweek; i++) {
+        var d = document.createElement("div");
+        d.classList.add("day");
+        d.classList.add("blank");
+        document.getElementById("calendarDays").appendChild(d);
+    }
+
+    for (var i = 0; i < num; i++) {
+        var tmp = i + 1;
+        var d = document.createElement("div");
+        d.id = "calendarday_" + tmp;
+        d.className = "day";
+        d.innerHTML = tmp;
+        d.dataset.day = tmp;              // easier to retrieve the date
+
+        /* ****************** Click Event ********************** */
+        d.addEventListener('click', function(){
+            this.classList.toggle('selected');
+
+            if (!selectedDays.includes(this.dataset.day))
+                selectedDays.push(this.dataset.day);
+
+            else
+                selectedDays.splice(selectedDays.indexOf(this.dataset.day), 1);
+        });
+        /* **************************************************** */
+
+        document.getElementById("calendarDays").appendChild(d);
+    }
+
+    var clear = document.createElement("div");
+    clear.className = "clear";
+    document.getElementById("calendarDays").appendChild(clear);
+}
+
+
+
+
+
+     
+
